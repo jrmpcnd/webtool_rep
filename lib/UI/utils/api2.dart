762d931c687a4a5ff139ca51cdc66b734d3a02a1
FDSAP-Joshua <111028254+Uwa1999@users.noreply.github.com>
2023-02-22 13:25:15 +0800

    API 2

--- lib/UI/utils/api2.dart
+++ lib/UI/utils/api2.dart
@@ -1,15 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../../core/providers/data_provider.dart';
 import 'model2.dart';
 
 class HierachyPush {
   Future<http.Response> pushHttp2() async {
     http.Response response2 = await http.post(
-      Uri.parse('http://10.21.0.74:1234/get_hierarchy'),
+      Uri.parse('$API/get_hierarchy'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization':
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
       },
       body: jsonEncode(
         <String, String>{
@@ -45,15 +46,14 @@ class HierarchyParse_Api {
 class RemittanceL_Push {
   Future<http.Response> pushHttp2() async {
     http.Response response2 = await http.post(
-      Uri.parse('http://10.21.0.74:1234/get_remittancelog'),
+      Uri.parse('$API/get_remittancelog'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization':
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
       },
       body: jsonEncode(
         <String, String>{
-
           "Cancelled_By_Fullname": "",
           "Disbursed_By_Fullname": "",
           "Processed_By_Fullname": "",
@@ -96,31 +96,28 @@ class RemittanceL_Parse {
   }
 }
 
-
 class ListofUserDevice_Push {
   Future<http.Response> pushHttp2() async {
     http.Response response2 = await http.post(
-      Uri.parse('http://10.21.0.74:1234/get_listuseddevice'),
+      Uri.parse('$API/get_listuseddevice'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization':
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njk5NDQ0NjAsImlzQWRtaW4iOnRydWUsInVzZXIiOnsiY2lkIjpudWxsLCJtb2JpbGUiOm51bGwsInVzZXJuYW1lIjpudWxsfX0.uzPKB5VQ_Ru_Z0LdA49cz4QUT8pOCVCeiX8LVSV2AHE'
       },
       body: jsonEncode(
         <String, String>{
-
-
-            "created_date": "",
-            "device_id": "",
-            "device_model": "",
-            "android_version": "",
-            "cid": "",
-            "branch_code": "",
-            "mobile_number": "",
-            "client_name": "",
-            "client_type": "",
-            "device_status": ""
-          },
+          "created_date": "",
+          "device_id": "",
+          "device_model": "",
+          "android_version": "",
+          "cid": "",
+          "branch_code": "",
+          "mobile_number": "",
+          "client_name": "",
+          "client_type": "",
+          "device_status": ""
+        },
       ),
     );
     if (response2.statusCode == 200) {
@@ -138,7 +135,8 @@ class ListofUserDevice_Parse {
     ListofUserDevice_Push httpPush2 = ListofUserDevice_Push();
     http.Response res2 = await httpPush2.pushHttp2();
     print("-------->>>>>>>>>>${jsonDecode(res2.body).length}");
-    var useoflistdevicelog = UseoflistDevice_Api.fromJson(jsonDecode(res2.body));
+    var useoflistdevicelog =
+        UseoflistDevice_Api.fromJson(jsonDecode(res2.body));
     return useoflistdevicelog;
   }
 }
