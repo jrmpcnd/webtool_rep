79cf0378cc989aabcb20658d10729c181ee36095
FDSAP-Joshua <111028254+Uwa1999@users.noreply.github.com>
2023-05-24 15:51:34 +0800

    product type fix

--- lib/UI/screens/utilities/producttype/product_type.dart
+++ lib/UI/screens/utilities/producttype/product_type.dart
@@ -492,7 +492,7 @@ class MyData2 extends DataTableSource {
   @override
   DataRow getRow(int index) {
     debugPrint(index.toString());
-    return DataRow(cells: [
+    return const DataRow(cells: [
       DataCell(
           SizedBox(child: Text('No Data Found, Please Enter Valid Keyword'))),
       DataCell(SizedBox(child: Text(''))),
@@ -514,7 +514,7 @@ class MyData3 extends DataTableSource {
   @override
   DataRow getRow(int index) {
     debugPrint(index.toString());
-    return DataRow(cells: [
+    return const DataRow(cells: [
       DataCell(SizedBox(child: Text('Loading Please wait!'))),
       DataCell(SizedBox(child: Center(child: CircularProgressIndicator()))),
       DataCell(SizedBox(child: Center(child: CircularProgressIndicator()))),
