edc4936d5e2a9fc54418b12d528dd446cd51d994
FDSAP-Joshua <111028254+Uwa1999@users.noreply.github.com>
2023-06-20 11:28:19 +0800

    role management

--- lib/UI/screens/administration/rolemanagement/role_management.dart
+++ lib/UI/screens/administration/rolemanagement/role_management.dart
@@ -58,202 +58,203 @@ class _RolemanagementState extends State<Rolemanagement> {
     ScrollController scrollController = ScrollController();
     return Container(
       padding: kEdgeInsetsVerticalNormal,
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: kTertiaryColor5,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
+      child: Expanded(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: kTertiaryColor5,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3)),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3)),
-              ],
-            ),
-            height: 200.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
+              height: 130.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 35.0,
+                      width: 400,
+                      child: TextFormField(
+                        style: kTextStyle,
+                        decoration: const InputDecoration(
+                          hintText: 'Role Management',
+                          border: OutlineInputBorder(),
+                          labelStyle: TextStyle(fontSize: 12.0),
+                          contentPadding: EdgeInsets.only(left: 10.0),
+                          hintStyle: TextStyle(color: kSecondaryColor2),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: kBlackColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: kBlackColor),
+                          ),
+                        ),
+                        textInputAction: TextInputAction.go,
+                        controller: controller,
+                      ),
+                    ),
+                  ),
+                  // DropdownButton(value: init,items: res.map((e) {return DropdownMenuItem(value: e,child: Text(e, style: TextStyle(color: Colors.black)),);}).toList(), onChanged: (value) {
+                  //   setState(() {
+                  //     init = value.toString();
+                  //   });
+                  // },),
+                  verticalSpaceSmall,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
                       children: [
-                        Column(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             SizedBox(
+                              width: 100.0,
                               height: 35.0,
-                              width: 400,
-                              child: TextFormField(
-                                style: kTextStyle,
-                                decoration: const InputDecoration(
-                                  hintText: 'Role Management',
-                                  border: OutlineInputBorder(),
-                                  labelStyle: TextStyle(fontSize: 12.0),
-                                  contentPadding: EdgeInsets.only(left: 10.0),
-                                  hintStyle: TextStyle(color: kSecondaryColor2),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: kBlackColor),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(color: kBlackColor),
-                                  ),
-                                ),
-                                textInputAction: TextInputAction.go,
-                                controller: controller,
-                              ),
-                            ),
-                            verticalSpaceTiny,
-                            // DropdownButton(value: init,items: res.map((e) {return DropdownMenuItem(value: e,child: Text(e, style: TextStyle(color: Colors.black)),);}).toList(), onChanged: (value) {
-                            //   setState(() {
-                            //     init = value.toString();
-                            //   });
-                            // },),
-                            verticalSpaceSmall,
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 100.0,
-                                  height: 35.0,
-                                  child: ElevatedButton.icon(
-                                    style: ButtonStyle(
-                                        backgroundColor:
-                                        MaterialStateProperty.all(kPrimaryColor)),
-                                   onPressed: (){
-                                      try{
-                                        if (controller.text.isNotEmpty) {
-                                          setState(() {
-                                            isLoaded = false;
-                                          });
-                                          shared.role_data.clear();
-                                          for (var i in shared.role[0].data!) {
-                                            print(i.toJson());
-                                            print(i.role_name?.toLowerCase().contains(
-                                                controller.text.toLowerCase()));
-                                            if (i.toJson().isNotEmpty) {
-                                              if (i.role_name!.toLowerCase().contains(
-                                                  controller.text.toLowerCase())) {
-                                                debugPrint(i.role_name);
-                                                setState(() {
-                                                  shared.role_data
-                                                      .add(Data.fromJson(i.toJson()));
-                                                });
-                                                if (shared.role_data.isNotEmpty) {
-                                                  Future.delayed(
-                                                    Duration(seconds: 1),
-                                                        () {
-                                                      setState(() {
-                                                        isLoaded = true;
-                                                      });
-                                                    },
-                                                  );
-                                                }
-                                              }
-                                            }
-                                          }
-                                        }
-                                        debugPrint(
-                                            shared.role_data[0].toJson().toString());
-                                      }catch (e) {
-                                        shared.role_data.clear();
-                                        isLoaded = true;
-                                      }
-                                   },
-                                    icon: const Icon(
-                                      Icons.search,
-                                      size: 20.0,
-                                    ),
-                                    label: Text(
-                                      'Search',
-                                      style: kSmallRegularTextStyle,
-                                    ),
-                                  ),
-                                ),
-                                horizontalSpaceTiny,
-                                SizedBox(
-                                  width: 100.0,
-                                  height: 35.0,
-                                  child: ElevatedButton.icon(
-                                    style: ButtonStyle(
-                                        backgroundColor: MaterialStateProperty.all(
-                                            kSecondaryColor2)),
-                                    onPressed: () {
+                              child: ElevatedButton.icon(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        kPrimaryColor)),
+                                onPressed: () {
+                                  try {
+                                    if (controller.text.isNotEmpty) {
                                       setState(() {
                                         isLoaded = false;
                                       });
-                                      controller.clear();
                                       shared.role_data.clear();
-                                      setState(() {
-                                        shared.role_data.addAll(shared.role[0].data!);
-                                        Future.delayed(
-                                          Duration(seconds: 1),
-                                              () {
+                                      for (var i in shared.role[0].data!) {
+                                        print(i.toJson());
+                                        print(i.role_name
+                                            ?.toLowerCase()
+                                            .contains(
+                                                controller.text.toLowerCase()));
+                                        if (i.toJson().isNotEmpty) {
+                                          if (i.role_name!
+                                              .toLowerCase()
+                                              .contains(controller.text
+                                                  .toLowerCase())) {
+                                            debugPrint(i.role_name);
                                             setState(() {
-                                              isLoaded = true;
+                                              shared.role_data.add(
+                                                  Data.fromJson(i.toJson()));
                                             });
-                                          },
-                                        );
-                                      });
-                                    },
-                                    icon: const Icon(
-                                      Icons.refresh,
-                                      size: 20.0,
-                                    ),
-                                    label: Text(
-                                      'Reset',
-                                      style: kSmallRegularTextStyle,
-                                    ),
-                                  ),
+                                            if (shared.role_data.isNotEmpty) {
+                                              Future.delayed(
+                                                Duration(seconds: 1),
+                                                () {
+                                                  setState(() {
+                                                    isLoaded = true;
+                                                  });
+                                                },
+                                              );
+                                            }
+                                          }
+                                        }
+                                      }
+                                    }
+                                    debugPrint(shared.role_data[0]
+                                        .toJson()
+                                        .toString());
+                                  } catch (e) {
+                                    shared.role_data.clear();
+                                    isLoaded = true;
+                                  }
+                                },
+                                icon: const Icon(
+                                  Icons.search,
+                                  size: 20.0,
                                 ),
-                              ],
+                                label: Text(
+                                  'Search',
+                                  style: kSmallRegularTextStyle,
+                                ),
+                              ),
+                            ),
+                            horizontalSpaceTiny,
+                            SizedBox(
+                              width: 100.0,
+                              height: 35.0,
+                              child: ElevatedButton.icon(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        kSecondaryColor2)),
+                                onPressed: () {
+                                  setState(() {
+                                    isLoaded = false;
+                                  });
+                                  controller.clear();
+                                  shared.role_data.clear();
+                                  setState(() {
+                                    shared.role_data
+                                        .addAll(shared.role[0].data!);
+                                    Future.delayed(
+                                      Duration(seconds: 1),
+                                      () {
+                                        setState(() {
+                                          isLoaded = true;
+                                        });
+                                      },
+                                    );
+                                  });
+                                },
+                                icon: const Icon(
+                                  Icons.refresh,
+                                  size: 20.0,
+                                ),
+                                label: Text(
+                                  'Reset',
+                                  style: kSmallRegularTextStyle,
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
-
-          Container(
-              width: double.infinity,
-              padding: kEdgeInsetsVerticalNormal,
-              child: Theme(
-                data: Theme.of(context)
-                    .copyWith(cardColor: Color(0xFF6F8A71),),
-
-
-                child: PaginatedDataTable(
-                  key: key,
-                  arrowHeadColor: kWhiteColor,
-                  columns: [
-                    DataColumn(
-                        label: Text('Role Name', style: kLargeBoldTextStyle)),
-                    DataColumn(
-                        label: Text('Role Desc', style: kLargeBoldTextStyle))
-                  ],
-                  source: isLoaded
-                      ? shared.role_data.isNotEmpty
-                          ? data
-                          : data2
-                      : data3,
-                  rowsPerPage: 8,
-                  showFirstLastButtons: true,
-                  header: Text('List of Role', style: kXLargeBoldTextStyle),
-                ),
-              )),
-        ],
+            Container(
+                width: double.infinity,
+                padding: kEdgeInsetsVerticalNormal,
+                child: Theme(
+                  data: Theme.of(context).copyWith(
+                    cardColor: Color(0xFF6F8A71),
+                  ),
+                  child: PaginatedDataTable(
+                    key: key,
+                    arrowHeadColor: kWhiteColor,
+                    columns: [
+                      DataColumn(
+                          label: Text('Role Name', style: kLargeBoldTextStyle)),
+                      DataColumn(
+                          label: Text('Role Desc', style: kLargeBoldTextStyle))
+                    ],
+                    source: isLoaded
+                        ? shared.role_data.isNotEmpty
+                            ? data
+                            : data2
+                        : data3,
+                    rowsPerPage: 8,
+                    showFirstLastButtons: true,
+                    header: Text('List of Role', style: kXLargeBoldTextStyle),
+                  ),
+                )),
+          ],
+        ),
       ),
     );
   }
